c36ab7763c17b20e2563d52fcd66f925137c771c
TRUVIHIU\hieup <top1taker@gmail.com>
2022-08-10 12:23:19 +0700

    traning day 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nodejs-traning
 # day 1
 https://drive.google.com/file/d/1r8B9qKbFL8nwJifAPwleyZgMijJ5F5g3/view?usp=sharing
+# day 2
+https://drive.google.com/file/d/1dHjPF8n7M9OiAF-yPT1QtUbDlpq9FVIC/view?usp=sharing
--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 const express = require('express')
 const http = require('http')
+const jwt = require('jsonwebtoken')
 
 const app = express()
-app.use(express.json())
+
+app.use(express.json()) // middleware
+
+const SECRET_KEY_JWT = 'very_secret_key' 
+// create middleware
+const checkAuth = (req, res, next) => {
+        const token = req.headers['authorization']
+        const decoded = jwt.decode(token, SECRET_KEY_JWT) 
+        if (decoded) {
+            req.user = decoded
+            return next()
+        } else {
+            res.status(401).json({message: 'Not authenticated'})
+        }
+}
 
 const server = http.createServer(app)
 
-let todos = [{id: 1, title: 'todo1'}, {id: 2, title: 'todo2'}] 
+let todos = [
+{
+    "id": "1",
+    "title": "todo 1",
+    "completed": false,
+    "user": "1"
+},
+{
+    "id": "2",
+    "title": "todo 2",
+    "completed": false,
+    "user": "1"
+},
+{
+    "id": "3",
+    "title": "todo 3",
+    "completed": false,
+    "user": "1"
+}] 
 
-app.get('/todos', (req, res)=> {
-    return res.status(200).json(todos)
+app.get('/todos', checkAuth, (req, res)=> {
+    const userId = req.user.id
+    const todosOfUser = todos.filter(todo => todo.user === userId)
+    return res.status(200).json(todosOfUser)
 })
 
-app.post('/todos', (req, res) => {
+app.post('/todos', checkAuth,  (req, res) => {
     const newTodo = req.body
+    newTodo.user = req.user.id
     todos = [...todos, newTodo] // ...todos 
-    // todos = [{title: 'todo1'}, {title: 'todo2'} ]
-    // ...todos = {title: 'todo1'}, {title: 'todo2'} => [...todos, newTodo] => [{title: 'todo1'}, {title: 'todo2'}, newTodo ]
-    // todos.push(newTodo)
     return res.status(200).json({message: 'Create todo successfully'})
 })
 
@@ -29,10 +62,42 @@ app.delete('/todos/:todoId', (req,res)=> {
     return res.status(200).json({message: 'Delete todo successfully'}) 
 })
 
-app.get('todos/:todoId', (req,res)=> {
-    
+app.get('/todos/:todoId', (req,res)=> {
+    const {todoId} = req.params 
+    const todo = todos.find(todo => todo.id === todoId) // find # filter 
+    if (!todo) return res.json({})
+    return res.json(todo)
 })
 // app.post()
+
+app.put('/todos/:todoId', (req,res)=> {
+    const {todoId} = req.params 
+    const updatedTodo = req.body
+    todos = todos.map(todo => todo.id === todoId ? updatedTodo : todo)
+    return res.json({message: 'Updated todo successfully'})
+})
+
+let users = [{id: '1', email: 'admin@gmail.com', fullName: 'admin', password: 'admin'}]
+
+app.post('/auth/register', (req, res) => {
+    const user = req.body 
+    users = [...users, user]
+    res.json({message: 'Registered successfully'})
+})
+
+app.post('/auth/login', (req, res) => {
+    const {email, password} = req.body 
+    const found = users.find(user => user.email === email && user.password === password) 
+    if (found) {
+        const token = jwt.sign({id: found.id, email: found.email}, SECRET_KEY_JWT)
+        return res.json({token, email: found.email})
+    }
+    res.status(401).json({message: 'Logged in failed'})
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
 
 const port = 9999;
 
